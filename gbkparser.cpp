f06275810510c04881a7fb0063fc7ad3e7bf8eec
Denis Gorev <eveleech@gmail.com>
2016-04-13 19:19:39 +0300

    fix type for debug locus date

--- gbkparser.cpp
+++ gbkparser.cpp
@@ -221,8 +221,9 @@ void GbkParser::parseTopLevel(const QString &prefix, QString value, SequencePtr 
         const QStringList words = value.split(QRegExp("\\s+"));
         seq->refSeqId = words[0];
         seq->length = words[1].toUInt();
-        seq->gbk_date = words[6];
-        qDebug() << "date: " << seq->gbk_date;
+        //seq->gbk_date = words[6];
+        //qDebug() << "date: " << seq->gbk_date;
+        qDebug() << "date: " << words[6];
         qDebug() << "... " << seq->refSeqId
                  << " from " << _fileName
                  << " by worker " << QThread::currentThreadId();
