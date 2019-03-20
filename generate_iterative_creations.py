c75b03413d92a72959afea80b8b48323362c359b
jleech <eveleech@gmail.com>
2019-03-21 02:17:31 +0300

    create/drop databases

--- generate_iterative_creations.py
+++ generate_iterative_creations.py
@@ -7,10 +7,19 @@ organisms = ["Ailuropoda_melanoleuca","Ananas_comosus","Apis_florea","Asparagus_
 scripts_path = "/home/i/Documents/introns-db-fill"
 increment_template_script = os.path.join(scripts_path, "iterative_create_database.sql")
 template = open(increment_template_script, "r").read()
-templates_path = "/home/i/Documents/introns-db-fill/iterative_creations"
+templates_path = os.path.join(scripts_path, "iterative_creations")
+
+
+start_script = os.path.join(templates_path, "start.sh")
+create_databases_script = os.path.join(templates_path, "create_databases.sql")
+drop_databases_script = os.path.join(templates_path, "drop_databases.sql")
+
+start = ""
+create_database_template = ""
+drop_databases_template = ""
 
 for idx, org in enumerate(organisms):
-	print("=========================================")
+	# print("=========================================")
 
 	print("{0} =============".format(org))
 	org_template = template.replace("%", org)
@@ -18,5 +27,21 @@ for idx, org in enumerate(organisms):
 	org_template_path = os.path.join(templates_path, org+".sql")
 	with open(org_template_path, "w") as org_template_file:
 		org_template_file.write(org_template)
-	print("=========================================\n\n")
+	# print("=========================================\n\n")
 
+	
+	start_template = "mysql --user=root --database="+org+" --password=1"
+	start_template = "{0} < {1}\n".format(start_template, org_template_path)	
+	start += start_template
+
+	create_database_template += "CREATE DATABASE " + org + ";\n"
+	drop_databases_template += "DROP DATABASE IF EXISTS " + org + ";\n"
+
+with open(start_script, "w") as start_script_file:
+		start_script_file.write(start)
+
+with open(create_databases_script, "w") as create_databases_script_file:
+		create_databases_script_file.write(create_database_template)
+
+with open(drop_databases_script, "w") as drop_databases_script_file:
+		drop_databases_script_file.write(drop_databases_template)
